4df4f7750a369f737eb85dbf269c4f59ad0a7566
gigitoto8 <gradu2a8nge@aol.com>
2024-09-25 06:50:49 +0000

    特定ビューでCSSが適用できるよう、ヘッダに@yield('custom_styles')を挿入

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,6 +4,7 @@
         <meta charset='utf-8'>
         <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
         <meta name='csrf-token' content='{{ csrf_token() }}'>       <!-- ログイン関係で追加 -->
+        @yield('custom_styles') <!-- ここでカスタムスタイルを挿入 -->
         <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css' >
         <title>TK_Portfolio</title>						<!--ウインドウ　タイトル-->
         <style>body {padding-top: 80px;}</style>    <!--余白-->
