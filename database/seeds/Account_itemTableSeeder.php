71a1768ced519409abf3460952f119445f9d068d
gigitoto8 <gradu2a8nge@aol.com>
2024-09-25 13:35:03 +0000

    値挿入

--- database/seeds/Account_itemTableSeeder.php
+++ database/seeds/Account_itemTableSeeder.php
@@ -11,6 +11,7 @@ class Account_itemTableSeeder extends Seeder
      */
     public function run()
     {
+        
         DB::table('account_items')->insert([
             [
                 'id' => '00001',
